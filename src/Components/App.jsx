e158c347fcca9b885b610a249db2ae85da3302e8
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-11-05 14:37:32 +0000

    view individual articles

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import ArticlesPage from './ArticlePage';
+import SingleArticle from './SingleArticle';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/articles/:article_id" element={<SingleArticle/>}/>
       </Routes>
     </>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import { Home } from 'lucide-react'
 
 const Header = () => {
   return (
     <header id="header">
+      <Link to="/" className="home-button">
+        <Home size={24} />
+      </Link>
       <div className="header-content">
       <h1>NC News</h1>
       <nav className ="desktop-nav">
